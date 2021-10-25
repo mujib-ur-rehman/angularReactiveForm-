2fc9176a70c82cd9c5e7d5130689b080a777bd25
mujib-ur-rehman <mujib.rehman@focusteck.com>
2021-10-25 16:24:21 +0500

    reactive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularReactiveForm';
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    food: new FormControl(''),
+
+    vehicle1: new FormControl(''),
+    vehicle2: new FormControl(''),
+    vehicle3: new FormControl('')
+  });
+  collectdata() {
+    console.warn(this.loginForm.value);
+  }
 }
